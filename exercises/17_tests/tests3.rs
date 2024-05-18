5854b80aac604168d59dc7cf52a7d1f7d9fa7ab1
Luka Colić <lcenzo@protonmail.ch>
2024-05-18 23:51:29 +0200

    Finish up to iterators

--- exercises/17_tests/tests3.rs
+++ exercises/17_tests/tests3.rs
@@ -7,8 +7,6 @@
 // Execute `rustlings hint tests3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 pub fn is_even(num: i32) -> bool {
     num % 2 == 0
 }
@@ -19,11 +17,15 @@ mod tests {
 
     #[test]
     fn is_true_when_even() {
-        assert!();
+		for i in 0..1000 {
+			assert!(is_even(2 * i));
+		}
     }
 
     #[test]
     fn is_false_when_odd() {
-        assert!();
+		for i in 0..1000 {
+			assert!(!is_even(2 * i + 1));
+		}
     }
 }
